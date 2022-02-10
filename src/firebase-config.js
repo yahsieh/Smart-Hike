bb107d5e2f2e1bb3052ba7e9811b106d1db82553
Venkata Sai Abhishek Kandikattu <vkandika@uci.edu>
2022-02-09 19:14:48 -0800

    ".env not working. Revert back to old method."
    Signed-off-by: Venkata Sai Abhishek Kandikattu <vkandika@uci.edu>

--- src/firebase-config.js
+++ src/firebase-config.js
@@ -1,14 +1,24 @@
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "@firebase/firestore";
 
+
 const firebaseConfig = {
-    apiKey: process.env.REACT_APP_API_KEY,
-    authDomain: process.env.REACT_APP_AUTH_DOMAIN,
-    projectId: process.env.REACT_APP_PROJECT_ID,
-    storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
-    messagingSenderId: process.env.REACT_APP_MESSAGE_SENDER_ID,
-    appId: process.env.REACT_APP_APP_ID
+    apiKey: "AIzaSyBJMjTkeMUIKYEZkaz2OeN86_6nY3yVnJs",
+    authDomain: "smart-hike.firebaseapp.com",
+    projectId: "smart-hike",
+    storageBucket: "smart-hike.appspot.com",
+    messagingSenderId: "876864580764",
+    appId: "1:876864580764:web:276389d599882ee5c05159"
   };
+
+// const firebaseConfig = {
+//     apiKey: process.env.REACT_APP_API_KEY,
+//     authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+//     projectId: process.env.REACT_APP_PROJECT_ID,
+//     storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+//     messagingSenderId: process.env.REACT_APP_MESSAGE_SENDER_ID,
+//     appId: process.env.REACT_APP_APP_ID
+//   };
   
 const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app)
