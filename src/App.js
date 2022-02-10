0f0bf3732ad2f272223dd2cd66fb222924cbc2f0
Venkata Sai Abhishek Kandikattu <vkandika@uci.edu>
2022-02-09 18:33:20 -0800

    basic project

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
 import './App.css';
-
+import { useState, useEffect } from 'react';
+import { db } from "./firebase-config";
+import { collection, doc, getDocs } from "firebase/firestore";
 function App() {
+  const [users, setUsers] = useState([]);
+  const usersCollectionRef = collection(db, "users")
+  useEffect(() =>{
+    const getUsers = async() => {
+      const data = await getDocs(usersCollectionRef);
+      console.log(data);
+      setUsers(data.docs.map((doc) => ({...doc.data(), id: doc.id})))
+    }
+    getUsers()
+  }, [])
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {users.map((user) => {
+        return <div> <h1> Name: {user.name}</h1></div>
+      })}
     </div>
   );
 }
