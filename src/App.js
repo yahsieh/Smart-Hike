f4827b2e8a799d1cd32caa7506a2f4562fdc11e1
Venkata Sai Abhishek Kandikattu <vkandika@uci.edu>
2022-02-14 00:01:33 -0800

    npm packages,router in index.js, routing in app.js
    Signed-off-by: Venkata Sai Abhishek Kandikattu <vkandika@uci.edu>

--- src/App.js
+++ src/App.js
@@ -2,25 +2,25 @@ import './App.css';
 import { useState, useEffect } from 'react';
 import { db } from "./firebase-config";
 import { collection, doc, getDocs } from "firebase/firestore";
-function App() {
-  const [users, setUsers] = useState([]);
-  const usersCollectionRef = collection(db, "users")
-  useEffect(() =>{
-    const getUsers = async() => {
-      const data = await getDocs(usersCollectionRef);
-      console.log(data);
-      setUsers(data.docs.map((doc) => ({...doc.data(), id: doc.id})))
-    }
-    getUsers()
-  }, [])
+import { Container, Row, Col } from 'react-bootstrap';
+import Login from "./components/Login"
+import Signup from "./components/Signup"
+import { Routes, Route } from "react-router-dom"
 
+
+function App() {
 
   return (
-    <div className="App">
-      {users.map((user) => {
-        return <div> <h1> Name: {user.name}</h1></div>
-      })}
-    </div>
+    <Container>
+      <Row>
+        <Col>
+        <Routes>
+          <Route path = "/" element = {<Login />} />
+          <Route path = "/signup" element = {<Signup />} />
+        </Routes>
+        </Col>
+      </Row>
+    </Container>
   );
 }
 
