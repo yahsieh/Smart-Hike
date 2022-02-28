1a5dda22c005b63f43ae97b2b28ffdeb948523e4
Venkata Sai Abhishek Kandikattu <vkandika@uci.edu>
2022-02-28 13:37:20 -0800

    Preferance Final
    Signed-off-by: Venkata Sai Abhishek Kandikattu <vkandika@uci.edu>

--- src/App.js
+++ src/App.js
@@ -11,6 +11,7 @@ import ApiTest from "./components/ApiTest"
 import { Routes, Route } from "react-router-dom"
 import { UserAuthContextProvider } from './context/UserAuthContext';
 import ProtectedRoute from "./context/ProtectedRoute";
+import PreferenceForm from "./components/PreferenceForm";
 
 function App() {
 
@@ -24,6 +25,7 @@ function App() {
             <Route path = "/signup" element = {<Signup />} />
             <Route path = "/home" element = {<ProtectedRoute> <Home /> </ProtectedRoute>} />
             <Route path = "/getdata" element = {<ApiTest />} />
+            <Route path="/preference" element={ <PreferenceForm /> } />
           </Routes>
         </UserAuthContextProvider> 
         </Col>
