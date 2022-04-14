c2ba396197c862362ba6f54227c0232877a196ba
AlteredReality <38482035+AlteredReality@users.noreply.github.com>
2022-04-14 16:20:06 -0700

    Updated DB code to connect to our DB
    Updated DB code to connect to our DB

--- src/firebase-config.js
+++ src/firebase-config.js
@@ -2,23 +2,25 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "@firebase/firestore";
 import { getAuth } from "firebase/auth";
 
-// const firebaseConfig = {
-//     apiKey: "AIzaSyBJMjTkeMUIKYEZkaz2OeN86_6nY3yVnJs",
-//     authDomain: "smart-hike.firebaseapp.com",
-//     projectId: "smart-hike",
-//     storageBucket: "smart-hike.appspot.com",
-//     messagingSenderId: "876864580764",
-//     appId: "1:876864580764:web:276389d599882ee5c05159"
-//   };
 
 const firebaseConfig = {
-    apiKey: process.env.REACT_APP_API_KEY,
-    authDomain: process.env.REACT_APP_AUTH_DOMAIN,
-    projectId: process.env.REACT_APP_PROJECT_ID,
-    storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
-    messagingSenderId: process.env.REACT_APP_MESSAGE_SENDER_ID,
-    appId: process.env.REACT_APP_APP_ID
-  };
+  apiKey: "AIzaSyAIw1295zWvO3CquZf_JbkTTX8GaAwulhE",
+  authDomain: "smarthiking-167c3.firebaseapp.com",
+  projectId: "smarthiking-167c3",
+  storageBucket: "smarthiking-167c3.appspot.com",
+  messagingSenderId: "153620858708",
+  appId: "1:153620858708:web:756b9cf11f1670ffceaac1",
+  measurementId: "G-82G6VZHJ7B"
+};
+
+// const firebaseConfig = {
+//     apiKey: process.env.REACT_APP_API_KEY,
+//     authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+//     projectId: process.env.REACT_APP_PROJECT_ID,
+//     storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+//     messagingSenderId: process.env.REACT_APP_MESSAGE_SENDER_ID,
+//     appId: process.env.REACT_APP_APP_ID
+//   };
   
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
