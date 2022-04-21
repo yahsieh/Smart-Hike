02e2e2405f8fab601f02cea2f7d8dcab81e07f0e
yahsieh <yunganh@uci.edu>
2022-04-20 21:55:38 -0700

    Add album to homepage

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
 
 import { Container, Row, Col } from 'react-bootstrap';
-import Login from "./components/Login"
-import Signup from "./components/Signup"
-import { Routes, Route } from "react-router-dom"
+import Home from "./components/Home";
+import Login from "./components/Login";
+import Signup from "./components/Signup";
+import { Routes, Route } from "react-router-dom";
 import { UserAuthContextProvider } from './context/UserAuthContext';
 import ProtectedRoute from "./context/ProtectedRoute";
 import PreferenceForm from "./components/PreferenceForm";
@@ -14,14 +15,15 @@ function App() {
     <Container>
       <Row>
         <Col>
-        <UserAuthContextProvider>
-          <Routes>
-            <Route path = "/" element = {<Login />} />
-            <Route path = "/signup" element = {<Signup />} />
-            <Route path = "/preference" element = {<ProtectedRoute> <PreferenceForm /> </ProtectedRoute>} />  
-            <Route path="/forgotPassword" element={ <ForgotPassword /> } />         
-          </Routes>
-        </UserAuthContextProvider> 
+          <UserAuthContextProvider>
+            <Routes>
+              <Route path="/" element={< Home />} />
+              <Route path="/login" element={<Login />} />
+              <Route path="/signup" element={<Signup />} />
+              <Route path="/preference" element={<ProtectedRoute> <PreferenceForm /> </ProtectedRoute>} />
+              <Route path="/forgotPassword" element={<ForgotPassword />} />
+            </Routes>
+          </UserAuthContextProvider>
         </Col>
       </Row>
     </Container>
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,21 +1,24 @@
 import React from "react";
 import { Button } from "react-bootstrap";
+import { PhotoAlbum } from "react-photo-album";
+import photos from "../photos";
 import { useUserAuth } from "../context/UserAuthContext";
 
-const Home= () => {
+
+const Home = () => {
     const { user, logout } = useUserAuth()
-    const handleLogout = async() => {
-        try{
+    const handleLogout = async () => {
+        try {
             await logout();
-        } catch(err) {
+        } catch (err) {
             console.log(err.message);
         }
     }
-    return(
+    return (
         <div>
-            <h1> Welcome {user.email}</h1>
+            {/* <h1> Welcome {user.email}</h1> */}
             <Button variant="primary" onClick={handleLogout}> Log out </Button>
-
+            <PhotoAlbum layout="columns" photos={photos} />
         </div>
     )
 }
